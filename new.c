8adc76e47185446434cd8f6c11e84852632bdf5c
yashkumar9084 <yash.ajay.cse.2025@miet.ac.in>
2026-07-30 12:08:37 +0530

    Add files via upload

---
+++ new.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int n;
+printf("Enter No.");
+scanf("%d",&n);
+printf("Table of %d is:\n",n);
+for(int i=1;i<=10;i++){
+    printf("%d * %d = %d\n",n,i,n*i);
+}
+return 0;
+}
