edd314d220733c243ee1c491d13b446b9bfd242c
yashkumar9084 <yash.ajay.cse.2025@miet.ac.in>
2026-09-16 12:03:37 +0530

    Create ll creation.c

---
+++ ll creation.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node {
+    int data;
+    struct node *next;
+
+};
+struct node* createnode(int data){ // Return type name()
+    struct node *p;
+    p = (struct node *)malloc(sizeof(struct node)); // sizeof is operator and (typecast)
+    if (p==NULL){
+        printf("Allocation error");
+
+    }
+    else{
+        p->data = data;
+        p->next = NULL;
+    }
+    return p;
+}
+void traversing(struct node *head){
+     struct node *t = head;
+    if (head==NULL)
+        printf("Empty Link list");
+    else{
+        while(t!=NULL){
+            printf("%d ",t->data);
+            t=t->next;
+        }
+    }
+}
+int main(){
+    struct node *p1,*p2,*head;
+    int data,i,n;
+    printf("Enter no of nodes you want to create: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        printf("Enter data for node: ");
+        scanf("%d",&data);
+        p1 = createnode(data);
+        if(i==1){
+            head = p1;
+            p2=p1;
+
+        }
+        else{
+            p2->next = p1;
+            p2=p1;
+        }
+    }traversing(head);
+    return 0;
+}
